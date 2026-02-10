7bf72aa76cb57bea215d2b0c9008896819c5ed03
Pratim <pratimhalder04@gmail.com>
2026-02-11 01:07:25 +0530

    Auth for signUp login logout Check done

---
+++ Backend/Src/index.js
@@ -0,0 +1,19 @@
+import dotenv from "dotenv";
+import express from "express";
+import { connectDB } from "./Lib/db.js";
+import cookieParser from "cookie-parser";
+import authRoutes from "./Routes/auth.route.js";
+
+dotenv.config();
+const app = express();
+
+const PORT = process.env.PORT;
+
+app.use(express.json());
+app.use(cookieParser());
+app.use("/api/auth", authRoutes);
+
+app.listen(PORT, ()=> {
+    console.log("Servers in running on Port: "+ PORT);
+    connectDB();
+});
